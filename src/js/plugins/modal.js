9436fdcd0d24d220b986339d04c9c36937b44f00
Developer <developer@Developers-iMac.local>
2020-11-09 22:17:59 +0300

    modal

---
+++ src/js/plugins/modal.js
@@ -0,0 +1,90 @@
+function _createModal(options) {
+    const DEFAULT_WIDTH = '500px'
+    const modal = document.createElement('div')
+    modal.classList.add('modal__inner')
+    modal.insertAdjacentHTML('afterbegin', `
+        <div class="modal-overlay" data-close = 'true'>
+            <div class="modal-window" style = 'width: ${options.width || DEFAULT_WIDTH}'>
+                <div class="modal-header">
+                    <span class="modal-title">${options.title || 'Window'}</span>
+                    ${options.closable ? `<span class="modal-close" data-close='true'>&times;</span>` : ''}
+                </div>
+                <div class="modal-body" data-content>
+                ${options.content || ''}
+                </div>
+                <div class="modal-footer">
+                    <button>Ok</button>
+                    <button>Cancel</button>
+                </div>
+            </div>
+        </div>
+    `)
+    document.body.appendChild(modal)
+    return modal
+}
+
+
+$.modal = function(options) {
+    const ANIMATION_SPEED = 200
+    const $modal = _createModal(options)
+    let closing = false
+    let destroyed = false
+    
+    const modal = {
+        open() {
+            if (destroyed){
+                return console.log('Modal is destroyed')
+            }
+            !closing && $modal.classList.add('open')
+        },
+        close() {
+            closing = true
+            $modal.classList.remove('open')
+            $modal.classList.add('hide')
+            setTimeout(() =>{
+                $modal.classList.remove('hide')
+                closing = false
+            }, ANIMATION_SPEED)
+        },
+    }
+
+const listener = event => {
+    if (event.target.dataset.close){
+        modal.close()
+    }
+}
+
+$modal.addEventListener('click', listener)
+  
+    return Object.assign(modal, {
+        destroy(){
+            $modal.parentNode.removeChild($modal)
+            $modal.removeEventListener('click', listener)
+            destroyed = true
+        },
+        setContent(html){
+            $modal.querySelector('[data-content').innerHTML = html
+        }
+    })
+}
+
+const modal = $.modal({
+    title:'modal window',
+    closable: true,
+    content: `
+    <p> Lorem ipsum dolor sit amet</p>
+    <p> Lorem ipsum dolor sit amet</p>
+    `,
+    width: '400px',
+    footerButtons: [{
+        text: 'Ok', type: 'primary', handler(){
+            console.log('primary btn')
+        }
+    }
+
+    ]
+})
+
+
+
+
